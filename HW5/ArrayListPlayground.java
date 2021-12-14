0986e63b6c0eda18438745fba1d4b4e33611ca33
GeorgeCamac <gcamac2015@gmail.com>
2021-12-13 22:28:45 -0500

    finished Arraylist playground

--- HW5/ArrayListPlayground.java
+++ HW5/ArrayListPlayground.java
@@ -1,4 +1,8 @@
-package HW5;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class ArrayListPlayground {
     public static void main(String[] args) {
@@ -9,7 +13,12 @@ public class ArrayListPlayground {
     // c) Set the value of the 5th element of the array alpha to 99.
     // d) Set the value of the 9th element of the array alpha to the sum of the 6th and 13th elements of the array nums.
 
-
+        ArrayList<Integer> q1 = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15));
+        System.out.println(q1.get(9));
+        q1.set(4,99);
+        System.out.println(q1.get(4));
+        q1.set(8, q1.get(5) + q1.get(12));
+        System.out.println(q1.get(8));
 
 
     // Question 2
@@ -18,7 +27,15 @@ public class ArrayListPlayground {
     // c) output the days of the week we have class
     // d) change the arrayList to start on Sunday
 
-
+        ArrayList<String> days = new ArrayList<>(Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"));
+        for(int i = 0; i < days.size(); i++){
+            System.out.println(days.get(i));
+        }
+        days.remove(6);
+        days.add(0,"Sunday");
+        for(int i = 0; i < days.size(); i++){
+            System.out.println(days.get(i));
+        }
     // Question 3 
     // a) create an ArrayList and prompt the user for numbers to add to it until the number 0 is selected
     // b) return the largest and smallest number
@@ -33,6 +50,55 @@ public class ArrayListPlayground {
     // ArrayList Size: 7
     // Please enter 2 more numbers to create the matrix...
     // 
+        ArrayList<Integer> userArray = new ArrayList<>();
+        Scanner keyboard = new Scanner(System.in);
+        int userInput = 1;
+        while(userInput != 0){
+            System.out.println("Input the integer to add to a list enter 0 when you are finished");
+            userInput = keyboard.nextInt();
+           if(userInput != 0){
+            userArray.add(userInput);
+           }
+        }
+        userInput = 1;
+        int smallest = userArray.get(0);
+        int largest = userArray.get(0);
+        for(int i = 0; i < userArray.size(); i++){
+            if(userArray.get(i) < smallest){
+                smallest = userArray.get(i);
+            }
+            if(userArray.get(i) > largest){
+                largest = userArray.get(i);
+            }
+        }
+        System.out.println("Largest input: " + largest);
+        System.out.println("Smallest input: " + smallest);
+        userArray.sort(null);
+        System.out.println(userArray);
+        Integer numsNeeded;
+        Integer extra = 0;
+        if(userArray.size() % 3 == 0){
+            for(int i = 0; i < userArray.size(); i += 3){
+                System.out.println(userArray.subList(i, i + 3));
+            }
+        }
+        else{
+            System.out.println("Please enter " + (3 - userArray.size() % 3) + " more numbers to create a matrix");
+            numsNeeded = (3 - userArray.size() % 3);
+            while(extra != numsNeeded){
+                System.out.println("Input the integer to add to a list enter 0 when you are finished");
+                userInput = keyboard.nextInt();
+               if(userInput != 0){
+                userArray.add(userInput);
+               }
+               extra++;
+            }
+            for(int i = 0; i < userArray.size(); i += 3){
+                System.out.println(userArray.subList(i, i + 3));
+            }
+        }
+        
+        
     }
    
 }
